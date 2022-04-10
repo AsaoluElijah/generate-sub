f7520c6e0b4d9848d1d0f015357fe61429fdc564
Asaolu Elijah <asaoluelijah7@gmail.com>
2022-04-10 17:28:58 +0100

    updated port

--- app.js
+++ app.js
@@ -13,4 +13,4 @@ app.get("/start-bot", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Example app listening on port 3000!"));
+app.listen(PORT, () => console.log("Example app listening on port 5000!"));

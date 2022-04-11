49ecbdbafa1faa768d62692887336beb515dbab0
Asaolu Elijah <asaoluelijah7@gmail.com>
2022-04-11 10:05:32 +0100

    update procfile

--- bot.js
+++ bot.js
@@ -41,71 +41,74 @@ const sendVideoTweet = async (toTweetId, tweetText, videoPath) => {
  */
 const streamTweets = async (tweetsFrom) => {
   console.log(`Started streaming tweets from ${tweetsFrom}`);
+  try {
+    const stream = await userClient.v1.filterStream({
+      track: tweetsFrom,
+    });
+    stream.autoReconnect = true;
 
-  const stream = await userClient.v1.filterStream({
-    track: tweetsFrom,
-  });
-  stream.autoReconnect = true;
+    stream.on(ETwitterStreamEvent.Data, async (tweet) => {
+      const senderName = tweet.user.screen_name;
+      const senderTweetId = tweet.id_str;
 
-  stream.on(ETwitterStreamEvent.Data, async (tweet) => {
-    const senderName = tweet.user.screen_name;
-    const senderTweetId = tweet.id_str;
+      console.log(`New mention from @${senderName} 🔔`);
 
-    console.log(`New mention from @${senderName} 🔔`);
+      // get preferred language from tweet
+      let language = tweet.text.trim().split(" ");
+      language = language[language.length - 1];
 
-    // get preferred language from tweet
-    let language = tweet.text.trim().split(" ");
-    language = language[language.length - 1];
+      console.log(`Language: ${language}`);
 
-    console.log(`Language: ${language}`);
+      // get original tweet
+      const ogTweet = await getTweet(tweet.in_reply_to_status_id_str);
+      // Check if original tweet contains a video
 
-    // get original tweet
-    const ogTweet = await getTweet(tweet.in_reply_to_status_id_str);
-    // Check if original tweet contains a video
+      if (
+        ogTweet.extended_entities &&
+        ogTweet.extended_entities.media[0].type === "video"
+      ) {
+        console.log("Original tweet contains a video 🎬");
 
-    if (
-      ogTweet.extended_entities &&
-      ogTweet.extended_entities.media[0].type === "video"
-    ) {
-      console.log("Original tweet contains a video 🎬");
+        // get video url with type mp4
+        const ogTweetVidURL =
+          ogTweet.extended_entities.media[0].video_info.variants.filter(
+            (res) => res.content_type === "video/mp4"
+          )[0].url;
 
-      // get video url with type mp4
-      const ogTweetVidURL =
-        ogTweet.extended_entities.media[0].video_info.variants.filter(
-          (res) => res.content_type === "video/mp4"
-        )[0].url;
+        // console.log("ogTweetVidURL: ", ogTweetVidURL);
 
-      // console.log("ogTweetVidURL: ", ogTweetVidURL);
+        const response = await generateTranscript({
+          outputLanguage: language,
+          videoInput: ogTweetVidURL,
+          isURL: true,
+        });
 
-      const response = await generateTranscript({
-        outputLanguage: language,
-        videoInput: ogTweetVidURL,
-        isURL: true,
-      });
+        // update generateTranscript to send response as object and do:
+        if (response.status === "success") {
+          const videoPath = response.video_path;
+          const srtPath = response.srt_path;
+          const tweetText = `Hi @${senderName} here's your video with translated transcript:`;
 
-      // update generateTranscript to send response as object and do:
-      if (response.status === "success") {
-        const videoPath = response.video_path;
-        const srtPath = response.srt_path;
-        const tweetText = `Hi @${senderName} here's your video with translated transcript:`;
-
-        const replyResult = sendVideoTweet(
-          senderTweetId,
-          tweetText,
-          videoPath,
-          srtPath
-        );
-        if (replyResult) {
-          console.log("Tweet sent ✅\n");
+          const replyResult = sendVideoTweet(
+            senderTweetId,
+            tweetText,
+            videoPath,
+            srtPath
+          );
+          if (replyResult) {
+            console.log("Tweet sent ✅\n");
+          }
         }
+      } else {
+        console.log("Original tweet does not contain a video 🚫");
+        // tweet does not contain video
       }
-    } else {
-      console.log("Original tweet does not contain a video 🚫");
-      // tweet does not contain video
-    }
-  });
+    });
+  } catch (error) {
+    console.log(`Error code: ${error.code}\nError: ${error.error}`);
+    // reconnect
+    // streamTweets(tweetsFrom);
+  }
 };
 
-module.exports = streamTweets;
-// streamTweets("@generate_sub");
-// sendVideoTweet("1508152780661342216", "./temp/output.mp4");
+streamTweets("@generate_sub");
